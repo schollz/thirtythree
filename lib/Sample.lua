0991270938f1f591cd9a24dd553b048aa5c8cb8e
Zack Scholl <zack.scholl@gmail.com>
2021-04-22 10:24:19 -0700

    fix

--- lib/Sample.lua
+++ lib/Sample.lua
@@ -1,6 +1,7 @@
 --
 -- Sample class
 -- contains information for sample
+-- includes splicing informatoin
 --
 
 Sample = {}
@@ -10,25 +11,77 @@ function Sample:new(o)
   setmetatable(o, self)
   self.__index = self
   self.__tostring = function(t) return t:to_string() end
-  o.loaded = false
   o.index_sc = o.index_sc or 1
+  o.splices = {}
+  for i=1,16 do 
+    o.splices[i]={
+      s=(i-1)*16,
+      e=i/16,
+      amp=1.0,
+      effect=0,
+    }
+  end
+  o.type == "melodic" or o.type
+  if o.type == "melodic" then
+    o.splices[1].e=1
+  end
+  
+  o.loaded = false
   return o
 end
 
 function Sample:to_string()
-  return self:get_filename()
-end
-
-function Sample:get_filename()
   return self.filename
 end
 
+-- Sample:load gets audio info and loads into supercollider
 function Sample:load(filename)
   -- loads sample
   self.name = filename:match("^.+/(.+).wav$")
   self.ch, self.samples, self.sample_rate = audio.file_info(filename)
   self.duration = samples / 48000.0
   self.filename = filename
-  self.rate_compensation = sample_rate / 48000.0 -- compensate for files that aren't 48Khz
-  -- TODO: load it into supercollider
+
+  -- load it into supercollider
+  engine.tt_load(self.index_sc,self.filename)
+  self.loaded=true
+
+  if mode_debug then 
+    print("loaded "..filename.." into sc slot "..self.index_sc)
+  end
 end
+
+
+-- Sample:play will play splice i
+function Sample:play(i,override)
+  local s = override.s or self.splices[i].s
+  local e = override.e or self.splices[i].e
+  if mode_debug then 
+    print("playing "..self.name.." on sc slot "..self.index_sc.." at pos ("..s..","..e..")")
+  end
+  engine.tt_play(
+    self.index_sc,
+    override.amp or self.splices[i].amp,
+    s,
+    e,
+    override.effect or self.splices[i].effect,
+  )
+end
+
+-- Sample:get_start_end converts from 0,1 to the actual duration
+-- and returns converted s,e and duration
+function Sample:get_start_end(s,e)
+  s = util.linlin(0,1,0,self.duration,s)
+  e = util.linlin(0,1,0,self.duration,e)
+  return s,e,e-s
+end
+
+function Sample:set_start_end(i,s,e)
+  if mode_debug then 
+    print("setting "..self.name.." on sc slot "..self.index_sc.." to pos ("..s..","..e..")")
+  end
+  self.splices[i].s=s
+  self.splices[i].e=e
+end
+
+return Sample
