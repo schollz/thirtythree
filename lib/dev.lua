d524be494902daab28a88fc8ebb422b20a59e87b
Zack Scholl <zack.scholl@gmail.com>
2021-04-27 20:35:54 -0700

    more sounds

--- lib/dev.lua
+++ lib/dev.lua
@@ -8,15 +8,25 @@ function Dev:new(o)
   self.__index=self
 
 
-  -- sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/bass_bpm70_amin.wav"
-  -- ops[1]:sound_load(9,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/bass_bpm70_amin.wav"
+  ops[1]:sound_load(9,sel_filename)
   sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/drums_bpm70.wav"
   ops[1]:sound_load(10,sel_filename)
-  -- sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/rhodes_bpm70_amin.wav"
-  -- ops[1]:sound_load(11,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/rhodes_bpm70_amin.wav"
+  ops[1]:sound_load(11,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/voice_bpm70_amin.wav"
+  ops[1]:sound_load(12,sel_filename)
 
-  -- sel_filename="/home/we/dust/audio/jams/jam1_amin_70bpm/voice_bpm70_amin.wav"
-  -- ops[1]:sound_load(12,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam2_amin/bass_amin.wav"
+  ops[1]:sound_load(13,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam2_amin/guitar_amin.wav"
+  ops[1]:sound_load(14,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam2_amin/strings_amin.wav"
+  ops[1]:sound_load(15,sel_filename)
+  sel_filename="/home/we/dust/audio/jams/jam2_amin/vocals_amin.wav"
+  ops[1]:sound_load(16,sel_filename)
+
+
   ops[1].cur_snd_id=10
   sel_adj=ADJ_TRIM
   -- snapshot:backup()
