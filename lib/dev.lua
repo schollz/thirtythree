17153549ba2792029ca10691b6024ccf8a215c4c
Zack Scholl <zack.scholl@gmail.com>
2021-05-02 07:32:38 +0700

    playing

--- lib/dev.lua
+++ lib/dev.lua
@@ -33,17 +33,17 @@ function Dev:new(o)
   -- sel_filename="/home/we/dust/audio/breakbeat/bpm120/beats8_bpm120_adt_120_drum_break_vinylised.wav"
   -- ops[1]:sound_load(11,sel_filename)
 
-  -- for i=1,4 do
-  --   sel_filename="/home/we/dust/code/thirtythree/defaults/yelidek_kit.wav"
-  --   ops[i]:sound_load(16,sel_filename)
-  --   sel_filename="/home/we/dust/code/thirtythree/defaults/steinway_c.wav"
-  --   ops[i]:sound_load(1,sel_filename)
-  -- end
+  for i=1,4 do
+    sel_filename="/home/we/dust/code/thirtythree/defaults/yelidek_kit.wav"
+    ops[i]:sound_load(16,sel_filename)
+    -- sel_filename="/home/we/dust/code/thirtythree/defaults/steinway_c.wav"
+    -- ops[i]:sound_load(1,sel_filename)
+    ops[i].cur_snd_id=16
+  end
 
-  ops[1].cur_snd_id=1
-  sel_adj=ADJ_TONE
+  sel_adj=ADJ_TRIM
   -- -- snapshot:backup()
-  snapshot:restore()
+  -- snapshot:restore()
   return o
 end
 
